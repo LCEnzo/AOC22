937af3e0711fcbff7080a6e8f19c795f4e613ba5
Luka Colić <lcenzo@protonmail.ch>
2023-09-24 11:36:58 +0200

    Map to tabs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Doing AOC22 to learn Rust.
+
 https://adventofcode.com/2022
--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,12 +1,23 @@
-use std::cmp::max;
 use std::fs::File;
 use std::path::Path;
 use std::io::{BufRead, BufReader};
+use itertools::Itertools;
+
+fn calc_score(opp: char, my: char) -> i32 {
+	let mut score = 0;
+
+	return score;
+}
 
 fn main() {
-    let input_path = Path::new("input.txt");
-    let input_file = File::open(input_path).expect("Could not open file input.txt");
-    let reader = BufReader::new(input_file);
+	let input_path = Path::new("input.txt");
+	let input_file = File::open(input_path).expect("Could not open file input.txt");
+	let reader = BufReader::new(input_file);
 
-
+	let it = vec![vec!['A', 'B', 'C'], vec!['X', 'Y', 'Z']].into_iter();
+	for v in Itertools::multi_cartesian_product(it) {
+		let score = calc_score(v[0], v[1]);
+		let str = v.iter().join(" ");
+		println!("{str}");
+	}
 }
