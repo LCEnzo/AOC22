3df54a63d08080e673c05cf5199fbbe9bc2c97cb
Luka Colić <lcenzo@protonmail.ch>
2023-09-25 23:05:19 +0200

    Finished second half, optimized first half.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -2,36 +2,94 @@ use itertools::Itertools;
 
 fn main() {
 	let input = include_str!("input.txt");
-	let mut sum: u32 = 0;
+	let mut rucksack_sum: u32 = 0;
+	let mut group_sum: u32 = 0;
+	let mut lines = input.lines().peekable();
 
-	for line in input.lines() {
-		if !line.is_empty() {
-			let rucksack_sum = sum_of_common_priorities_of_rucksack(line.to_string());
-			// println!("{rucksack_sum}");
-			sum += rucksack_sum;
+	while lines.peek() != None {
+		let line1 = lines.next();
+		if let Some(li) = line1 {
+			rucksack_sum += sum_of_common_priorities_of_rucksack(li.to_string());
+		} else {
+			break;
+		}
+
+		let line2 = lines.next();
+		if let Some(li) = line2 {
+			rucksack_sum += sum_of_common_priorities_of_rucksack(li.to_string());
+		} else {
+			break;
+		}
+
+		let line3 = lines.next();
+		if let Some(li) = line3 {
+			rucksack_sum += sum_of_common_priorities_of_rucksack(li.to_string());
+		} else {
+			break;
+		}
+
+		group_sum += find_group_score(line1.unwrap(), line2.unwrap(), line3.unwrap());
+	}
+
+	println!("\nrucksack_sum: {rucksack_sum}");
+	println!("group_sum: {group_sum}");
+}
+
+fn find_group_score(l1: &str, l2: &str, l3: &str) -> u32 {
+	let top = l1.chars().sorted().collect::<String>();
+	let mid = l2.chars().sorted().collect::<String>();
+	let bot = l3.chars().sorted().collect::<String>();
+
+	let mid = mid.as_bytes();
+	let bot = bot.as_bytes();
+	let mut mid_index = 0;
+	let mut bot_index = 0;
+
+	'outer: for c in top.chars() {
+		let mut c2 = mid[mid_index] as char;
+		let mut c3 = bot[bot_index] as char;
+
+		while c > c2 {
+			mid_index += 1;
+			if mid_index >= mid.len() {
+				break 'outer;
+			}
+
+			c2 = mid[mid_index] as char;
+		}
+
+		while c2 > c3 {
+			bot_index += 1;
+			if bot_index >= bot.len() {
+				break 'outer;
+			}
+
+			c3 = bot[bot_index] as char;
+		}
+
+		if c == c2 && c2 == c3 {
+			return type_to_score(c);
 		}
 	}
 
-	println!("\nSum: {sum}")
+	let top = l1.chars().sorted().collect::<String>();
+	let mid = l2.chars().sorted().collect::<String>();
+	let bot = l3.chars().sorted().collect::<String>();
+	println!("Intersection not found");
+	println!("\t - {top}");
+	println!("\t - {mid}");
+	println!("\t - {bot}");
+	panic!("Did not find common elements between 3 elves");
 }
 
 fn sum_of_common_priorities_of_rucksack(rucksack: String) -> u32 {
-	println!("\t {rucksack}");
 	let (comp1, comp2) = rucksack.split_at(rucksack.len() / 2);
-
-	find_common_elements(&comp1, &comp2)
-		.iter()
-		.map(|x| type_to_score(*x))
-		.fold(0, |acc, x| acc + x)
+	type_to_score(find_common_elements(&comp1, &comp2))
 }
 
-fn find_common_elements(left_side: &str, right_side: &str) -> Vec<char> { 
-	// println!("\t - intersection of {left_side} {right_side}:");
-	let mut intersection = vec![];
+fn find_common_elements(left_side: &str, right_side: &str) -> char { 
 	let left_side = left_side.chars().sorted().collect::<String>();
 	let right_side = right_side.chars().sorted().collect::<String>();
-
-	println!("\t - sorted          {left_side} {right_side} = | ");
 
 	let bytes = right_side.as_bytes();
 	let mut index = 0;
@@ -48,16 +106,11 @@ fn find_common_elements(left_side: &str, right_side: &str) -> Vec<char> {
 		}
 
 		if c == c2 {
-			intersection.push(c);
-			break;
+			return c;
 		}
 	}
 
-	let c = intersection[0];
-	let val = type_to_score(c);
-	println!("\t intersection = {c} = {val}\n");
-
-	intersection
+	panic!("Did not find common elements between 2 compartments of a ruckasck");
 }
 
 
