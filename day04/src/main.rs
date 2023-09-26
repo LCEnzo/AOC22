222fb82985f4f4fc10f59b9f60270e0c97afdc92
Luka Colić <lcenzo@protonmail.ch>
2023-09-26 20:11:49 +0200

    Day04 second half

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -2,7 +2,7 @@ use std::num::ParseIntError;
 
 fn main() {
     let input_txt = include_str!("input.txt");
-    let count = input_txt
+    let pairs_of_bounds = input_txt
         .lines()
         .filter_map(|line| {
             let pairs_of_section_bounds: Vec<_> = line.split(',')
@@ -16,6 +16,9 @@ fn main() {
                 None
             }
         })
+        .collect::<Vec<Vec<(u32, u32)>>>();
+
+    let contain_count = pairs_of_bounds.iter()
         .filter(|pair_of_bounds| {
             let (start1, end1) = pair_of_bounds[0];
             let (start2, end2) = pair_of_bounds[1];
@@ -27,7 +30,19 @@ fn main() {
         })
         .count();
 
-    println!("{}", count);
+    let overlap_count = pairs_of_bounds.iter()
+        .filter(|pair_of_bounds| {
+            let (start1, end1) = pair_of_bounds[0];
+            let (start2, end2) = pair_of_bounds[1];
+            let range1 = start1..=end1;
+            let range2 = start2..=end2;
+
+            range1.contains(&start2) || range1.contains(&end2) || range2.contains(&start1) || range2.contains(&end1)
+        })
+        .count();
+
+    println!("{}", contain_count);
+    println!("{}", overlap_count);
 }
 
 fn str_to_sections_bounds(section: &str) -> Result<(u32, u32), ParseIntError> {
