e0737ffedc9b7e643cbdb68913e9e43dd442c750
Luka Colić <lcenzo@protonmail.ch>
2023-12-02 15:59:00 +0100

    Solve day1 second half

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -1,8 +1,9 @@
 #![feature(portable_simd)]
 
-use std::simd::{u8x64, Mask, SimdPartialOrd, Simd, SimdUint};
+use std::simd::{u8x64, SimdPartialOrd, Simd};
 use rayon::str::ParallelString;
-use rayon::*;
+use rayon::iter::ParallelIterator;
+use std::time::Instant;
 
 fn pad_out_and_convert_line(line: &[u8]) -> u8x64 {
     const LEN: usize = 64;
@@ -71,16 +72,71 @@ fn vec_to_num(simd_val: u8x64) -> u32 {
 
 fn calc_solution_1(input: &str) -> u32 {
     input
-        .lines()
+        .par_lines()
         .map(|line| pad_out_and_convert_line(line.to_ascii_lowercase().as_bytes()))
         .map(|bytes| convert_char_digits_to_uint(bytes))
         .map(|bytes| vec_to_num(bytes))
         .sum()
 }
 
+// 56001 is too low
+fn digest_line(line: &str) -> u64 {
+    let digit_strs = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let mut index = 0;
+    let mut digits = vec![];
+
+    'outer: while index < line.len() {
+        // if index < line.len() {
+        //     println!("index: {}; line[index:]: {}", index, line.get(index..).unwrap());
+        // }
+        
+        for (s_index, s) in digit_strs.iter().enumerate() {
+            let l = s.len();
+            if index+l <= line.len() && **s == line[index..(index+l)] {
+                digits.push(s_index as u64 % 10);
+                index += 1; // l; // Wow, it's pretty meh to have overlapping digit strings. Dislike
+                continue 'outer;
+            }
+        }
+
+        index += 1;
+    }
+
+    // print!("digits: ");
+    // for digit in digits.clone() {
+    //     print!("{}, ", digit);
+    // } print!("\n");
+
+    let first = digits.first().unwrap();
+    let last = if digits.is_empty() { first } else { digits.last().unwrap() };
+
+    // print!("val: {}\n", first * 10 + last);
+
+    first * 10 + last
+}
+
+fn calc_solution_2(input: &str) -> u64 {
+    input
+        .par_lines()
+        .map(|line| digest_line(line))
+        .sum()
+}
+
 fn main() {
     let input = include_str!("input.txt");
-    println!("{}", calc_solution_1(input));
+    // For performance testing
+    // let input = input.repeat(100);
+    // let input = input.as_str();
+
+    let start = Instant::now();
+    let solution = calc_solution_1(input);
+    let elapsed = start.elapsed();
+    println!("1 took: {}s {}ms {}μs\nSolution:\n\t{}\n", elapsed.as_secs(), elapsed.subsec_millis(), elapsed.subsec_micros() % 1000, solution);
+
+    let start = Instant::now();
+    let solution = calc_solution_2(input);
+    let elapsed = start.elapsed();
+    println!("2 took: {}s {}ms {}μs\nSolution:\n\t{}\n", elapsed.as_secs(), elapsed.subsec_millis(), elapsed.subsec_micros() % 1000, solution);
 }
 
 #[cfg(test)]
@@ -125,5 +181,14 @@ mod tests {
     fn test_first_half() {
         let input = include_str!("test_input.txt");
         assert_eq!(142, calc_solution_1(input));
+    }
+
+    #[test]
+    fn test_second_half() {
+        let input = include_str!("test_input.txt");
+        assert_eq!(142, calc_solution_2(input));
+
+        let input = include_str!("test_input2.txt");
+        assert_ne!(281, calc_solution_2(input));
     }
 }
